aa168c3d90f5dfc4bd00c93cb17624508a9263c2
Jordan Hudgens <jordan@wow.com>
2017-12-16 12:27:05 -0700

    Added validations for the support requests

--- app/models/support_request.rb
+++ app/models/support_request.rb
@@ -2,4 +2,11 @@ class SupportRequest < ApplicationRecord
   belongs_to :user
 
   has_many :support_request_comments, dependent: :destroy
+
+  validates_presence_of :title, :content, :category
+
+  enum status: {
+    unresolved: 0,
+    resolved:   1,
+  }
 end
--- app/models/support_request_comment.rb
+++ app/models/support_request_comment.rb
@@ -1,4 +1,6 @@
 class SupportRequestComment < ApplicationRecord
   belongs_to :support_request
   belongs_to :user
+
+  validates_presence_of :comment
 end

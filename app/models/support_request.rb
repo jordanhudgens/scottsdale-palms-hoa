9d5a3d58879b0f57354971fd30e4f6d560ff5c34
Jordan Hudgens <jordan@wow.com>
2017-12-16 12:32:16 -0700

    Built out enum for support request categories

--- app/models/support_request.rb
+++ app/models/support_request.rb
@@ -3,10 +3,17 @@ class SupportRequest < ApplicationRecord
 
   has_many :support_request_comments, dependent: :destroy
 
-  validates_presence_of :title, :content, :category
+  validates_presence_of :title, :content
 
   enum status: {
     unresolved: 0,
     resolved:   1,
+  }
+
+  enum request_category: {
+    general_inquiry:   0,
+    repair_request:    1,
+    policy_violation:  2,
+    landscaping_issue: 3,
   }
 end

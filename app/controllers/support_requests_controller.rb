0a3c1b0a0ad19d81051ed6c79a2549186fe83ce8
Jordan Hudgens <jordan@wow.com>
2017-12-16 13:38:56 -0700

    Integrated form for the support request new and edit screens

--- app/controllers/support_requests_controller.rb
+++ app/controllers/support_requests_controller.rb
@@ -1,30 +1,25 @@
 class SupportRequestsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_support_request, only: [:show, :edit, :update, :destroy]
+  layout 'full_page'
 
-  # GET /support_requests
-  # GET /support_requests.json
   def index
     @support_requests = SupportRequest.all
   end
 
-  # GET /support_requests/1
-  # GET /support_requests/1.json
   def show
   end
 
-  # GET /support_requests/new
   def new
     @support_request = SupportRequest.new
   end
 
-  # GET /support_requests/1/edit
   def edit
   end
 
-  # POST /support_requests
-  # POST /support_requests.json
   def create
     @support_request = SupportRequest.new(support_request_params)
+    @support_request.user = current_user
 
     respond_to do |format|
       if @support_request.save
@@ -37,8 +32,6 @@ class SupportRequestsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /support_requests/1
-  # PATCH/PUT /support_requests/1.json
   def update
     respond_to do |format|
       if @support_request.update(support_request_params)
@@ -51,8 +44,6 @@ class SupportRequestsController < ApplicationController
     end
   end
 
-  # DELETE /support_requests/1
-  # DELETE /support_requests/1.json
   def destroy
     @support_request.destroy
     respond_to do |format|
@@ -62,13 +53,16 @@ class SupportRequestsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_support_request
       @support_request = SupportRequest.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def support_request_params
-      params.require(:support_request).permit(:title, :content, :user_id, :status, :category)
+      params.require(:support_request).permit(
+        :title,
+        :content,
+        :status,
+        :request_category,
+      )
     end
 end

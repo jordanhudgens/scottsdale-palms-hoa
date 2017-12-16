173552327c57185ab148b7b41591951f48d84fdd
Jordan Hudgens <jordan@wow.com>
2017-12-16 15:28:47 -0700

    Built out helper method to prettify and list support request categories

--- app/helpers/support_requests_helper.rb
+++ app/helpers/support_requests_helper.rb
@@ -1,2 +1,9 @@
 module SupportRequestsHelper
+  def request_category_array
+    request_category_keys = SupportRequest.request_categories.keys
+
+    request_category_keys.each_with_object([]) do |k, arr|
+      arr << [k.gsub('_', ' ').titleize, k]
+    end
+  end
 end

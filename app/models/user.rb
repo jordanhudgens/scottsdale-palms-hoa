bbc0b6157fb3ecfb4cd155b29edfcb40d0dca4d9
Jordan Hudgens <jordan@wow.com>
2017-12-16 12:15:08 -0700

    Wired up relationship for support requests

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :newsletters
+  has_many :newsletters, dependent: :destroy
+  has_many :support_requests, dependent: :destroy
 end

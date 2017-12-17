f1a2e3f529c471e3eed50376b891f5e5ddff0993
Jordan Hudgens <jordan@wow.com>
2017-12-16 21:26:38 -0700

    Integrated the weather API and styled the homepage

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,5 +1,9 @@
 class StaticController < ApplicationController
   def home
+    require 'open_weather'
+    weather_options = { units: "imperial", APPID: ENV.fetch('WEATHER_API_ID') }
+    weather_obj = OpenWeather::Current.geocode(33.4942, -111.9261, weather_options)
+    @temperature = weather_obj['main']['temp'].to_s.split('.').first
   end
 
   def contact_us

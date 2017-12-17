efdee32564115af5ae5f36311f8ccbbb1ab06dc0
Jordan Hudgens <jordan@wow.com>
2017-12-16 20:12:26 -0700

    Added role and full name to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ class User < ApplicationRecord
   has_many :newsletters, dependent: :destroy
   has_many :support_requests, dependent: :destroy
   has_many :support_request_comments, dependent: :destroy
+
+  enum role: {
+    standard: 0,
+    admin:    0,
+  }
 end
